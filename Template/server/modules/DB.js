ce51624004873c5a97646f778d9620a7f6b4c092
GiuseppeCaruso <101046629+GiuseppeCaruso@users.noreply.github.com>
2022-10-16 18:41:56 +0200

    Add files via upload

--- Template/server/modules/DB.js
+++ Template/server/modules/DB.js
@@ -1,29 +1,71 @@
 'use strict';
 
 const sqlite = require('sqlite3');
+const crypto = require('crypto');
 
-class DatabaseConnection {
-    static db = new sqlite.Database("./queueManager.db", (err) => { if (err) throw err; });
+//open db
+const db = new sqlite.Database('./OfficeQueueManagement.db', (err) => {
+  if(err) throw err;
+});
 
-    static async createConnection() {
-        await this.createTablePositions();
 
-    }
+//get the full list of available courses
+exports.getServicesPerOfficer = () => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM ServicesPerOfficer';
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const officer_services = rows.map((r) => ({ id: r.id, idOfficer: r.idOfficer, idService: r.idService}));
+      resolve(officer_services);
+    });
+  });
+};
 
-    static createTablePositions() {
-        return new Promise(async (resolve, reject) => {
-           // const sql = "CREATE TABLE IF NOT EXISTS Positions (positionID TEXT PRIMARY KEY UNIQUE NOT NULL, aisle TEXT, row TEXT, col TEXT, maxWeight INTEGER, maxVolume INTEGER, occupiedWeight INTEGER, occupiedVolume INTEGER);";
-            this.db.run(sql, [], function (err) {
-                if (err)
-                    reject(err);
-                else {
-                    resolve('Tables created');
-                }
-            });
-        });
-    }
+//get the full list of available services
+exports.getServices = () => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM Services';
+      db.all(sql, [], (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        const services = rows.map((r) => ({ id: r.id, name: r.name, tr: r.tr}));
+        resolve(services);
+      });
+    });
+  };
 
-    
-}
+//get queues
+exports.getQueues = () => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM Queue';
+      db.all(sql, [], (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        const queues = rows.map((r) => ({ userId: r.userId, idService: r.idService, ticketTime: r.ticketTime, turnTime: r.turnTime}));
+        resolve(queues);
+      });
+    });
+  };
 
-module.exports = DatabaseConnection;
+// add a new user when a service is selected
+exports.addUserToQueue = (idService, idUser) => {
+    return new Promise((resolve, reject) => {
+      const sql = 'INSERT INTO Queue (userId, idService, tickeTime, turnTime) VALUES(?, ?, ?, ?)';
+      db.run(sql, [idUser, idService, 0, 0], function (err) {//null values must be filled with future implementation
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(null);
+      });
+    });
+  };
+
+  
